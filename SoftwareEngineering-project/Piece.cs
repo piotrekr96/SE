496d7c2abbde0f39985929b6c7a27658ecfcd499
piotrekr96 <piotrek_x@vp.pl>
2017-11-11 21:59:05 +0100

    Add files via upload

---
+++ SoftwareEngineering-project/Piece.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineering_project
+{
+    public class Piece
+    {
+        protected int position_x, position_y;
+        protected bool sham = false;
+        protected Player owner = null;
+        
+        
+        public Piece() : base()
+        {
+            // randomly turn the piece into a sham
+            int r = MyGlobals.rnd.Next(0, 2);
+            if (r == 1) {               
+                sham = true;
+            }
+
+            // make a piece and plce it if possible
+            do
+            {
+                position_x = MyGlobals.rnd.Next(0, MyGlobals.Width);
+                position_y = MyGlobals.rnd.Next(MyGlobals.smallHeight, MyGlobals.Height-MyGlobals.smallHeight-1);
+            } while (!placePieceInit(position_x, position_y));
+        }
+
+
+        public bool placePieceInit(int x, int y)
+        {
+
+            // if a piece with same coords is already there, return failure
+            foreach (Piece item in MyGlobals.pieces)
+            {
+                if (x == item.getPosX() && y == item.getPosY())
+                {
+                    return false;
+                }
+            }
+
+            // if there is no duplicate, place the piece and return success
+            MyGlobals.pieces.Add(this);
+            return true;
+        }
+
+        // look up the piece in the list
+        public static Piece findPiece(int x, int y) {
+            foreach (Piece item in MyGlobals.pieces) {
+                if (x == item.position_x && y == item.position_y) {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+
+        // setter and getters
+        public void setOwner(Player pl) {
+            this.owner = pl;
+        }
+        public void setPosX(int x) {
+            this.position_x = x;
+        }
+        public void setPosY(int y) {
+            this.position_y = y;
+        }
+        public Player getOwner() {
+            return this.owner;
+        }
+        public int getPosX()
+        {
+            return position_x;
+        }
+        public int getPosY()
+        {
+            return position_y;
+        }
+        public bool getSham()
+        {
+            return sham;
+        }
+
+
+    }
+}
--- SoftwareEngineering-project/Player.cs
+++ SoftwareEngineering-project/Player.cs
@@ -14,13 +14,14 @@ namespace SoftwareEngineering_project
     {
         protected int position_x, position_y;
         protected Bitmap bmp;
+        protected Piece carrying = null;
 
         public Player()
         {
             bmp = Properties.Resources.test;
         }
 
-        // setters and getters for x and y coords
+        // setters and getters 
         public int getPosX()
         {
             return position_x;
@@ -34,6 +35,9 @@ namespace SoftwareEngineering_project
         }
         public void setPosY(int y) {
             position_y = y;
+        }
+        public Piece getCarrying() {
+            return carrying;
         }
 
         public abstract Bitmap getBitmap();
@@ -58,6 +62,7 @@ namespace SoftwareEngineering_project
             return true;
         }
 
+        // movement also updates coords of pieces if they are being carried by the player that is moving
         public bool MoveUp()
         {
 
@@ -69,6 +74,12 @@ namespace SoftwareEngineering_project
 
             // if it can move, set new position
             this.setPosY(getPosY() - 1);
+
+            // if the player carries a piece, update piece location as well
+            if (this.carrying != null) {
+                //new coord Y of piece will be equal to new coord Y of player
+                carrying.setPosY(this.getPosY()); 
+            }
 
             // return success
             return true;
@@ -86,6 +97,14 @@ namespace SoftwareEngineering_project
             // if it can move, set new position
             this.setPosY(getPosY() + 1);
 
+            // if the player carries a piece, update piece location as well
+            if (this.carrying != null)
+            {
+                //new coord Y of piece will be equal to new coord Y of player
+                carrying.setPosY(this.getPosY());
+            }
+
+
             // return success
             return true;
         }
@@ -101,6 +120,13 @@ namespace SoftwareEngineering_project
             // if it can move, set new position
             this.setPosX(getPosX() - 1);
 
+            // if the player carries a piece, update piece location as well
+            if (this.carrying != null)
+            {
+                //new coord X of piece will be equal to new coord X of player
+                carrying.setPosX(this.getPosX());
+            }
+
             // return success
             return true;
         }
@@ -115,6 +141,13 @@ namespace SoftwareEngineering_project
 
             // if it can move, set new position
             this.setPosX(getPosX() + 1);
+
+            // if the player carries a piece, update piece location as well
+            if (this.carrying != null)
+            {
+                //new coord X of piece will be equal to new coord Y of player
+                carrying.setPosX(this.getPosX());
+            }
 
             // return success
             return true;
@@ -137,15 +170,120 @@ namespace SoftwareEngineering_project
 
         public abstract bool withinPlayerBounds(int y);
 
+        public bool canPlacePlayer(int x, int y)
+        {
+
+            // if a player or piece with same coords is already there, return failure
+            foreach (Player item in MyGlobals.redPlayers)
+            {
+                if (x == item.getPosX() && y == item.getPosY())
+                {
+                    return false;
+                }
+            }
+            foreach (Player item in MyGlobals.bluePlayers)
+            {
+                if (x == item.getPosX() && y == item.getPosY())
+                {
+                    return false;
+                }
+            }
+            foreach (Piece item in MyGlobals.pieces)
+            {
+                if (x == item.getPosX() && y == item.getPosY())
+                {
+                    return false;
+                }
+            }
+
+
+            // if there is no duplicate return success
+            return true;
+        }
+
+        // try to pick piece from own location
+        public bool pickPiece() {
+            Piece pi = Piece.findPiece(position_x, position_y);
+
+            // if the piece does not exist, return failure
+            if (pi == null) {
+                return false;
+            }
+
+            // if the piece exists but it is carried by someone else, return failure
+            if (pi.getOwner() != null) {
+                return false;
+            }
+
+            //if the piece exists and is not carried by another player, pick it
+            this.carrying = pi;
+            pi.setOwner(this);
+            return true;
+        }
+
+        // does not take coords
+        // a player can test a piece it it has been picked by him
+        public bool testPiece() {
+            return this.carrying.getSham();
+        }
+
+        public abstract bool canPlacePiece(int x, int y);
+
+        // can be placed without checking it is a sham
+        // returns true if at (x,y) it was a goal
+        // feedback is returned in discoverGoal()
+        public void placePiece(int x, int y)
+        {
+            // place piece at coords and update attributes
+            this.carrying.setPosX(x);
+            this.carrying.setPosY(y);
+            this.carrying.setOwner(null);
+            this.carrying = null;
+        }
+
+        public bool tryPlacePiece(int x, int y) {
+            if (!canPlacePiece(x, y))
+            {
+                Console.WriteLine("Index for placing on goal area is out of bounds!");
+                return false;
+            }
+
+            // if check passed, place piece 
+            bool wasSham = this.carrying.getSham();
+            placePiece(x, y);
+
+            // placing a piece which is a sham results in getting no information
+            if (wasSham) {
+                Console.WriteLine("TESTING PURPOSES: The piece was a sham, but the piece has been placed!");
+                return true;
+            }
+            // get feedback
+            if (discoverGoal(x, y))
+            {
+                Console.WriteLine("There was goal at those coordinates, and the piece has been placed!");
+            }
+            else {
+                Console.WriteLine("There was no goal at those coordinates, but the piece has been placed!");
+            }
+            return true;
+        }
+
+        public abstract bool discoverGoal(int x, int y);
+
     }
 
     public class BluePlayer : Player
     {
-        Random rnd = new Random();
         public BluePlayer() : base()
         {
-            position_x = rnd.Next(0, MyGlobals.Width);
-            position_y = rnd.Next(MyGlobals.Height - MyGlobals.smallHeight, MyGlobals.Height);
+            // try to place player in tasks area
+            do
+            {
+                position_x = MyGlobals.rnd.Next(0, MyGlobals.Width);
+                position_y = MyGlobals.rnd.Next(MyGlobals.smallHeight, MyGlobals.Height - MyGlobals.smallHeight - 1);
+
+            } while (!canPlacePlayer(position_x, position_y));
+
             MyGlobals.bluePlayers.Add(this);
             //   bmp = Properties.Resources.test;
         }
@@ -164,15 +302,58 @@ namespace SoftwareEngineering_project
             return true;
         }
 
+        public override bool canPlacePiece(int x, int y)
+        {
+            // if x coord out out bounds, return failure
+            if (x < 0 || x > (MyGlobals.Width - 1))
+            {
+                return false;
+            }
+
+            int min = MyGlobals.Height - MyGlobals.smallHeight;
+            int max = MyGlobals.Height - 1;
+
+            // if y coord is out of blue goals area bounds, return failure
+            if (y < min || y > max)
+            {
+                return false;
+            }
+
+            // if all checks pass, return success
+            return true;
+        }
+
+        public override bool discoverGoal(int x, int y)
+        {
+            // check if cell at coords holds a goal or non-goal
+            foreach (Goal item in MyGlobals.goalsBlue)
+            {
+
+                // if there exists a goal at (x,y) and it has not been discovered yet, discover it
+                if (item.getPosX() == x && item.getPosY() == y && item.getDiscovered() != true)
+                {
+                    item.setDiscovered();
+                    return true;
+                }
+            }
+
+            // if at (x,y) there was no goal, return failure
+            return false;
+        }
+
     }
 
     public class RedPlayer : Player
     {
-        Random rnd = new Random();
        public RedPlayer() : base()
         {
-            position_x = rnd.Next(0, MyGlobals.Width);
-            position_y = rnd.Next(0, MyGlobals.smallHeight);
+            // try to place player in tasks area
+            do {
+                position_x = MyGlobals.rnd.Next(0, MyGlobals.Width);
+                position_y = MyGlobals.rnd.Next(MyGlobals.smallHeight, MyGlobals.Height - MyGlobals.smallHeight - 1);
+    
+            } while(!canPlacePlayer(position_x, position_y));
+
             MyGlobals.redPlayers.Add(this);
 
         }
@@ -189,8 +370,43 @@ namespace SoftwareEngineering_project
             return true;
         }
 
-        
+        public override bool canPlacePiece(int x, int y)
+        {
+            // if x coord out out bounds, return failure
+            if (x < 0 || x > (MyGlobals.Width - 1))
+            {
+                return false;
+            }
 
+            int max = MyGlobals.smallHeight - 1;
+
+            // if y coord is out of red goals area bounds, return failure
+            if (y < 0 || y > max)
+            {
+                return false;
+            }
+
+            // if all checks pass, return success
+            return true;
+        }
+
+        public override bool discoverGoal(int x, int y)
+        {
+            // check if cell at coords holds a goal or non-goal
+            foreach (Goal item in MyGlobals.goalsRed)
+            {
+
+                // if there exists a goal at (x,y) and it has not been discovered yet, discover it
+                if (item.getPosX() == x && item.getPosY() == y && item.getDiscovered() != true)
+                {
+                    item.setDiscovered();
+                    return true;
+                }
+            }
+
+            // if at (x,y) there was no goal, return failure
+            return false;
+        }
 
 
 
