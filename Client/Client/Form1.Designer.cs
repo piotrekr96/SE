4731b6e9e34c95e580970c240734290626b0380f
piotrekr96 <piotrek_x@vp.pl>
2017-12-10 13:37:17 +0100

    test xyz

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(0, 3);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(281, 20);
+            this.Box.TabIndex = 0;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(83, 113);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(115, 23);
+            this.Connect.TabIndex = 1;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Box;
+        private System.Windows.Forms.Button Connect;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            Thread mThread = new Thread(new ThreadStart(ConnectAsClient));
+            mThread.Start();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    MessageBox.Show(ip.ToString());
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void ConnectAsClient()
+        {
+            TcpClient client = new TcpClient();
+
+            client.Connect(IPAddress.Parse(GetLocalIPAddress()), 5004);
+
+            NetworkStream stream =  client.GetStream();
+            string s = "Hello from Client";
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            stream.Write(message, 0, message.Length);
+            updateUI("Message send");
+
+
+            stream.Close();
+            client.Close();
+        }
+
+        private void updateUI(string s)
+        {
+            Func<int> del = delegate()
+            {
+                Box.AppendText(s + System.Environment.NewLine);
+                return 0;
+            };
+
+            Invoke(del);
+        }
+    }
+}
