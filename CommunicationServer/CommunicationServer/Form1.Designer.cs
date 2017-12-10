00ce8d178bd2339e7ac9b97a973d32869f7f6476
piotrekr96 <piotrek_x@vp.pl>
2017-12-10 13:39:24 +0100

    automatically gets local ip address

---
+++ CommunicationServer/CommunicationServer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace CommunicationServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Box = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(0, 0);
+            this.Box.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(284, 20);
+            this.Box.TabIndex = 0;
+            this.Box.TextChanged += new System.EventHandler(this.Box_TextChanged);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(61, 216);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Button);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Box;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
